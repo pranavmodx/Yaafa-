e8c8b574decaf77322af78b49f2474a4ecfe449c
Varun Krishna S <varunhawk19@gmail.com>
2019-01-20 10:09:54 +0530

    Update models.py

--- Hackathon/hackathon_app/models.py
+++ Hackathon/hackathon_app/models.py
@@ -16,6 +16,7 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}','{self.email}')"
 
 
+
 class super_super_user(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -43,15 +44,30 @@ class super_user(db.Model, UserMixin):
 class topics(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     topic_name = db.Column(db.String(20), nullable=False)
-
+    
+    def json(self):
+        return jsonify(
+            id=id,
+            topic_name=topic_name
+        )
 class subjects(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject_name = db.Column(db.String(20), nullable=False)
     topics=db.Column(db.Text(30000),nullable=False)
+    
+    def json(self):
+        a=topics.split(',')
+        return jsonify(
+            id=id,
+            subject_name=subject_name,
+            topics=a
+
+        )
 
 class Study_event(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
+    subject_name=db.Column(db.String(20),unique=True,nullable=False)
     topic_name=db.Column(db.String(20), nullable=False)
     pomodoro=db.Column(db.Boolean,nullable=False)
     ambient_music=db.Column(db.Boolean,nullable=False)
@@ -59,11 +75,31 @@ class Study_event(db.Model):
     _exit = db.Column(db.Boolean, nullable=False)
     proof_work=db.Column(db.Text(30000),nullable=False)
     summary = db.Column(db.Text(30000), nullable=False)
+    time=db.Column(db.DateTime, nullable=False)
+    def json(self):
+        return jsonify(
+            name=name,
+            subject_name=subject_name,
+            topic_name=topic_name,
+            pomodoro=pomodoro,
+            ambient_music=ambient_music,
+            intended_use=intended_use,
+            _exit=_exit,
+            proof_work=proof_work,
+            summary=summary,
+            time=time
+        )
 
 class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     book_name = db.Column(db.String(20), nullable=False)
     book_level = db.Column(db.Integer, primary_key=True)
+
+    def json(self):
+        return jsonify(
+            book_name=book_name,
+            book_level=book_level
+        )
 
 class problem_event(db.Model):
     id=db.Column(db.Integer,primary_key=True)
@@ -77,10 +113,25 @@ class problem_event(db.Model):
     proof_work=db.Column(db.Text(30000),nullable=False)
     no_of_probs = db.Column(db.Integer, primary_key=True)
     summary = db.Column(db.Text(30000), nullable=False)
-
+    time=db.Column(db.DateTime,nullable=False)
+    
+    def json(self):
+        return jsonify(
+            id=id,
+            name=name,
+            topic_name=topic_name,
+            book_name=book_name,
+            pomodoro=pomodoro,
+            ambient_music=ambient_music,
+            intended_use=intended_use,
+            _exit=_exit,
+            proof_work=proof_work,
+            no_of_probs=no_of_probs,
+            summary=summary,
+            time=time,
+        )
 class revise(db.Model):
     id=db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable=False)
     topic_name=db.Column(db.String(20), nullable=False)
     last_checked_on = db.Column(db.String(20), nullable=False)
     pomodoro=db.Column(db.Boolean,nullable=False)
@@ -89,4 +140,19 @@ class revise(db.Model):
     _exit = db.Column(db.Boolean, nullable=False)
     proof_work=db.Column(db.Text(30000),nullable=False)
     summary = db.Column(db.Text(30000), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    def json(self):
+        return jsonify(
+            id=id,
+            topic_name=topic_name,
+            last_checked_on=last_checked_on,
+            pomodoro=pomodoro,
+            ambient_music=ambient_music,
+            intended_use=intended_use,
+            proof_work=proof_work,
+            summary=summary,
+            time=time,
+
+        )
+
 
